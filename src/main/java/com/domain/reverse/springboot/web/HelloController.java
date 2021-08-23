9adf4b83acaa310850844c060dff658ba237473b
dongwoon <goldeastcloud@gmail.com>
2021-08-23 23:56:54 +0900

    롬복 테스트 1. gradle 롬복 추가 2. 모든 응답 Dto를 수용할 Dto패키지 생성 3. Dto 관련 테스트

--- src/main/java/com/domain/reverse/springboot/web/HelloController.java
+++ src/main/java/com/domain/reverse/springboot/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.domain.reverse.springboot.web;
 
+import com.domain.reverse.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController //1.컨트롤러를 JSON을 반환하는 컨트롤러로 만들어준다.
@@ -10,5 +12,11 @@ public class HelloController {
                             // 2.예전에는 @RequestMapping(method = RequestMethod.GET)으로 사용되었다.
     public String hello() { ///hello로 요청이 오면 문자열 Hello World!!를 반환함.
         return "Hello World!!";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, //외부에서 API로 넘긴 파라미터를 가져옴
+                                     @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
     }
 }
--- src/test/java/com/domain/reverse/springboot/web/HelloControllerTest.java
+++ src/test/java/com/domain/reverse/springboot/web/HelloControllerTest.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
+
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)    //JUnit 내장 실행자 외에 다른 실행자(SpringRunner)를 실행시킴. 스프링부트 테스트와 JUnit의 연결자 역할
@@ -27,5 +29,19 @@ public class HelloControllerTest {
         mvc.perform(get("/hello"))  //MockMvc를 통해 /hello 주소로 HTTP GET 요청, 체이닝 지원
                 .andExpect(status().isOk()) //mvc.perform의 결과 중 HTTP Header의 Status검증(200, 404, 500 등). 200이면 통과
                 .andExpect(content().string(hello));    //mvc.perform의 결과 중 응답 본문 내용 검증
+    }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(
+                get("/hello/dto")
+                        .param("name", name)    //API 테스트 시 사용될 요청 파라미터 설정, String만 가능
+                        .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))    //JSON 응답 값을 필드별로 검증
+                .andExpect(jsonPath("$.amount", is(amount)));
     }
 }
