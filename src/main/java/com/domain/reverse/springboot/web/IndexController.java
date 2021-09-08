a3fe34d087c992a9c18275f680f3a3498dca7750
dongwoon <goldeastcloud@gmail.com>
2021-09-08 15:53:59 +0900

    hotfix-4.2 / IndexController web하위로 이동

--- src/main/java/com/domain/reverse/springboot/web/IndexController.java
+++ src/main/java/com/domain/reverse/springboot/web/IndexController.java
@@ -1,4 +1,4 @@
-package com.domain.reverse.springboot.web.dto;
+package com.domain.reverse.springboot.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
