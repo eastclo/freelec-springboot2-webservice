812fc355b28443f76b09108006c2381416f90406
dongwoon <goldeastcloud@gmail.com>
2021-09-30 19:05:40 +0900

    5.7 테스트 기능 수정3 - hello관련 수정 - WebMvcTest에서 CustomOAuth2UserService를 스캔하지 못하므로 ScurityConfig를 스캔대상에서 제거하여 문제 해결 - EnableJpaAuditing을 분리

---
+++ src/main/java/com/domain/reverse/springboot/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.domain.reverse.springboot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing //JPA Auditing 활성화
+public class JpaConfig {
+}
